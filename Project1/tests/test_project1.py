b29e3a4ea99a1a57cc1517176841ec1cd26a0ac5
dragi <dkupenov@uci.edu>
2026-02-01 23:46:13 -0800

    Add tests for initialization and propagation

--- Project1/tests/test_project1.py
+++ Project1/tests/test_project1.py
@@ -1,6 +1,6 @@
 import unittest
 from pathlib import Path
-
+from collections import defaultdict
 from simulation import Simulation
 from device import Device
 
@@ -36,6 +36,36 @@ class Project1Test(unittest.TestCase):
         sim.read_file()
         self.assertEqual(sim.alert_queue, {'0': [('A', '1', [(2, 750)], 'Trouble')],
              '2200': [('C', '1', [(2, 750)], 'Trouble')]})
+
+    def test_alerts_can_be_sent(self):
+        test_queue = defaultdict(list)
+        device = Device(1)
+        device.send([(2, 300)], 'Test', 0, test_queue)
+        self.assertEqual(test_queue, {'300': [('RA', 2, 1, 'Test')]})
+
+    def test_device_initialization(self):
+        device = Device(5)
+        self.assertEqual(device.device_id, 5)
+        self.assertEqual(device.recipient_list, [])
+        self.assertEqual(device.message_list, [])
+        self.assertEqual(device.cancelled_messages, [])
+
+    def test_multiple_recipients_propagation(self):
+        test_queue = defaultdict(list)
+        device = Device(1)
+        device.send([(2, 100), (3, 200), (4, 300)], 'Test', 0, test_queue)
+        self.assertEqual(len(test_queue), 3)
+        self.assertIn(('RA', 2, 1, 'Test'), test_queue['100'])
+        self.assertIn(('RA', 3, 1, 'Test'), test_queue['200'])
+        self.assertIn(('RA', 4, 1, 'Test'), test_queue['300'])
+
+    def test_receive_alert_adds_to_message_list(self):
+        test_queue = defaultdict(list)
+        device = Device(2)
+        device.recipient_list = [(3, 100)]
+
+        device.receive_alert(1, 'Test', 100, test_queue)
+        self.assertIn('Test', device.message_list)
 
 if __name__ == '__main__':
     unittest.main()
