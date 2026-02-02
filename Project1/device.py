caa635439b6fc768bec480cad9c08e981018d5a6
dragi <dkupenov@uci.edu>
2026-02-01 16:43:19 -0800

    Split simulation code into separate functions

--- Project1/device.py
+++ Project1/device.py
@@ -2,3 +2,4 @@ class Device:
     def __init__(self, device_id: int):
         self.device_id = device_id
         self.recipients = []
+        self.alert_queue = []
--- Project1/simulation.py
+++ Project1/simulation.py
@@ -9,29 +9,38 @@ class Simulation:
         self.time = 0
         self.length = 1
 
-    def check_file_exists(self):
+    def check_file_exists(self) -> None:
         if not self.file_path.exists():
             print('FILE NOT FOUND')
             raise SystemExit
 
-    def read_file(self):
+    def read_file(self) -> None:
         with open(self.file_path) as f:
             line = f.readline()
             while line != '' and self.time < self.length:
                 instruction = line[0:1]
 
                 if instruction == 'L':
-                    simulation_length = line.split()[1]
-                    self.length = int(simulation_length)
+                    self.set_simulation_length(line)
                 elif instruction == 'D':
-                    device_id = line.split()[1]
-                    device = Device(int(device_id))
-                    self.devices[device_id] = device
+                    self.add_device(line)
                 elif instruction == 'P':
-                    line = line.split()
-                    sender = line[1]
-                    recipient = int(line[2])
-                    delay = int(line[3])
-                    self.devices[sender].recipients.append((recipient, delay))
+                    self.propagate_device(line)
 
                 line = f.readline()
+
+    def set_simulation_length(self, line: str) -> None:
+        simulation_length = line.split()[1]
+        self.length = int(simulation_length)
+
+    def add_device(self, line: str) -> None:
+        device_id = line.split()[1]
+        device = Device(int(device_id))
+        self.devices[device_id] = device
+
+    def propagate_device(self, line: str) -> None:
+        line = line.split()
+        sender = line[1]
+        recipient = int(line[2])
+        delay = int(line[3])
+        self.devices[sender].recipients.append((recipient, delay))
