51240b2c2d422199192eaaee0ff61298b69825f3
dragi <dkupenov@uci.edu>
2026-02-01 23:20:23 -0800

    Function doc strings and annotations

--- Project1/device.py
+++ Project1/device.py
@@ -1,29 +1,34 @@
 class Device:
     def __init__(self, device_id: int):
+        """A device is an object that has the ability to send, cancel, and receive alerts"""
         self.device_id = device_id
         self.recipient_list = []
         self.message_list = []
         self.cancelled_messages = []
 
-    def send(self, recipients: list, message: str, timestamp: int, queue: dict, devices: dict):
+    def send(self, recipients: list, message: str, timestamp: int, queue: dict) -> None:
+        """Send alert to all devices that were previously propagated and schedule reception"""
         for recipient, delay in recipients:
             time = str(int(timestamp) + delay)
             print(f'@{timestamp}: #{self.device_id} SENT ALERT TO #{recipient}: {message}')
             queue[time].append(('RA', recipient, self.device_id, message))
 
-    def cancel(self, recipients: list, message: str, timestamp: int, queue: dict):
+    def cancel(self, recipients: list, message: str, timestamp: int, queue: dict) -> None:
+        """Cancel alert to all devices that were previously propagated and schedule cancellation"""
         for recipient, delay in recipients:
             time = str(int(timestamp) + delay)
             print(f'@{timestamp}: #{self.device_id} SENT CANCELLATION TO #{recipient}: {message}')
             queue[time].append(('RC', recipient, self.device_id, message))
 
-    def receive_alert(self, sender: int, message: str, timestamp: int, queue: dict, devices: dict):
+    def receive_alert(self, sender: int, message: str, timestamp: int, queue: dict) -> None:
+        """Receive alert and forward it along the chain if it wasn't previously sent"""
         print(f'@{timestamp}: #{self.device_id} RECEIVED ALERT FROM #{sender}: {message}')
         if message not in self.message_list:
             self.message_list.append(message)
-            self.send(self.recipient_list, message, timestamp, queue, devices)
+            self.send(self.recipient_list, message, timestamp, queue)
 
-    def receive_cancellation(self, sender: int, message: str, timestamp: int, queue: dict, devices: dict):
+    def receive_cancellation(self, sender: int, message: str, timestamp: int, queue: dict) -> None:
+        """Cancel alert if it wasn't previously canceled"""
         print(f'@{timestamp}: #{self.device_id} RECEIVED CANCELLATION FROM #{sender}: {message}')
         if message in self.message_list and message not in self.cancelled_messages:
             self.cancelled_messages.append(message)
--- Project1/simulation.py
+++ Project1/simulation.py
@@ -1,9 +1,9 @@
 from pathlib import Path
 from collections import defaultdict
-
 from device import Device
 
 class Simulation:
+    """A simulation is an object that reads input files and manipulates Devices"""
     def __init__(self, file_path: Path):
         self.file_path = file_path
         self.devices = dict()
@@ -11,11 +11,13 @@ class Simulation:
         self.length = None
 
     def check_file_exists(self) -> None:
+        """Verifies whether the file exists, terminates program if it doesn't"""
         if not self.file_path.exists():
             print('FILE NOT FOUND')
             raise SystemExit
 
     def read_file(self) -> None:
+        """Reads files and manipulates Devices according to instructions"""
         with open(self.file_path) as f:
             line = f.readline()
             while line != '':
@@ -35,15 +37,18 @@ class Simulation:
                 line = f.readline()
 
     def set_simulation_length(self, line: str) -> None:
+        """Updates simulation length based on input file"""
         simulation_length = line.split()[1]
         self.length = int(simulation_length)
 
     def add_device(self, line: str) -> None:
+        """Adds a Device to the global list according to input file"""
         device_id = line.split()[1]
         device = Device(int(device_id))
         self.devices[device_id] = device
 
     def propagate_device(self, line: str) -> None:
+        """Propagates a Device according to input file and adds to recipient list"""
         line = line.split()
         sender = line[1]
         recipient = int(line[2])
@@ -51,6 +56,7 @@ class Simulation:
         self.devices[sender].recipient_list.append((recipient, delay))
 
     def add_to_queue(self, line: str, event_type: str) -> None:
+        """Adds an event to the event queue at the simulation time specified by input file"""
         line = line.split()
         sender = line[1]
         recipients = self.devices[sender].recipient_list[:]
@@ -59,6 +65,7 @@ class Simulation:
         self.alert_queue[simulation_time].append((event_type, sender, recipients, message))
 
     def run_simulation(self) -> None:
+        """Traverses event queue and dynamically updates it based on cancellations and receptions"""
         while self.alert_queue:
             timestamp = min(self.alert_queue.keys(), key = int)
 
@@ -75,7 +82,7 @@ class Simulation:
                     message = event[3]
                     if message not in self.devices[sender].message_list:
                         self.devices[sender].message_list.append(message)
-                    self.devices[sender].send(recipients, message, timestamp, self.alert_queue, self.devices)
+                    self.devices[sender].send(recipients, message, timestamp, self.alert_queue)
                 elif event_type == 'C':
                     sender = event[1]
                     recipients = event[2]
@@ -87,10 +94,12 @@ class Simulation:
                     recipient = event[1]
                     sender = event[2]
                     message = event[3]
-                    self.devices[str(recipient)].receive_alert(sender, message, timestamp, self.alert_queue, self.devices)
+                    self.devices[str(recipient)].receive_alert(sender, message, timestamp,
+                        self.alert_queue)
                 elif event_type == 'RC':
                     recipient = event[1]
                     sender = event[2]
                     message = event[3]
-                    self.devices[str(recipient)].receive_cancellation(sender, message, timestamp, self.alert_queue, self.devices)
+                    self.devices[str(recipient)].receive_cancellation(sender, message, timestamp,
+                        self.alert_queue)
         print(f'@{self.length}: END')
